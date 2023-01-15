14c8b6ab812ecbe784440a40a9cf12e541bbbffb
Jack Hurt <jackwhurt@gmail.com>
2023-01-15 16:40:56 +0000

    added white highlight

--- amplify-app/src/app/app-routing.module.ts
+++ amplify-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'features', component: AppComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home' }  // TODO: redirect to 404 component
 ];
